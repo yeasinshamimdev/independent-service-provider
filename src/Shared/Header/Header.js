0dfb2b8daa607e00773fb5b9e6fe430d285ead93
Shamim <yeasinshamim7@gmail.com>
2022-04-17 08:17:56 +0600

    header done

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import CustomLink from '../CustomLink/CustomLink';
 import './Header.css';
 
 const Header = () => {
     const navigate = useNavigate();
     return (
-        <div className="header-container">
-            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-                <Container>
-                    <h1 onClick={() => navigate('/')} className='header-title'>GYM WITH SHAMIM</h1>
+        <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
+            <Container>
+                <h1 onClick={() => navigate('/')} className='header-title'>GYM WITH SHAMIM</h1>
 
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" >
-                        <Nav className="me-auto">
-                            <Link to='/home'>Home</Link>
-                            <Link to="/services">Service</Link>
-                            <Link to="/blogs">Blogs</Link>
-                            <Link to="/about">About</Link>
-                        </Nav>
-                        <Nav>
-                            <Link to="/signup">Sign Up</Link>
-                            <Link to="login">Login</Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" >
+                    <Nav className="me-auto">
+                        <CustomLink to='/home'>Home</CustomLink>
+                        <CustomLink to="/services">Service</CustomLink>
+                        <CustomLink to="/blogs">Blogs</CustomLink>
+                        <CustomLink to="/about">About</CustomLink>
+                    </Nav>
+                    <Nav>
+                        <CustomLink to="/signup">Sign Up</CustomLink>
+                        <CustomLink to="login">Login</CustomLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
