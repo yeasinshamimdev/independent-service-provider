bcbe7f2a30ef37cd36fda9158a81f6c3be1fe306
Shamim <yeasinshamim7@gmail.com>
2022-04-18 22:02:46 +0600

    about page done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import developer from '../../images/developer.jpg';
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h3>This is about</h3>
+        <div className='about-container my-5 px-5'>
+            <div className='image-container px-4 mx-auto d-flex justify-content-center'>
+                <img className='w-75' src={developer} alt="" />
+            </div>
+            <div >
+                <h1 className='text-center mt-3'>Hi there,</h1>
+                <h2 className='text-center mb-4'>I am Yeasin Shamim</h2>
+                <p className='text-justify'>I have big dream to be a full stack web developer. I have already start this journey with programming hero community. My skills is:- HTML, CSS, Bootstrap, tailwind css, javaScript, Regex, React Js, Firebase, Material UI, Figma, React firebase hooks etc. I am very hard working person. I love pain. Because without pain you can't gain anything. So I want to very hard work for be a web developer. </p>
+            </div>
         </div>
     );
 };
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>blogs</h2>
+        <div className='d-flex'>
+            <div>
+                <h4>What is the difference between authorization and authentication</h4>
+                <h6>Authentication:</h6>
+                <li>Au</li>
+            </div>
+            <div>
+                <h4>Why are you using firebase? What other options do you have to implement authentication?</h4>
+                <p></p>
+            </div>
+            <div>
+                <h4>What other services does firebase provide other than authentication</h4>
+                <p></p>
+            </div>
         </div>
     );
 };
